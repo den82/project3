17d0bf58f92cc0c9c559db6725e77de26b7f26cc
Denis Tereshchuk <d.tereshchuk@gmail.com>
2022-07-13 13:35:10 +0300

    installed annotations, made VinylController

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response {
+        return new Response('Title: bla bla');
+    }
+}
