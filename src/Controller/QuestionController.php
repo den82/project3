fbf373ca147287bbdf39b3737b308d85ecdbddb6
Denis Tereshchuk <d.tereshchuk@gmail.com>
2022-07-15 15:15:36 +0300

    New class 'QuestionControlloer' and route for main page

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage() {
+        //die('Start');
+        return new Response('Start!');
+    }
+}
